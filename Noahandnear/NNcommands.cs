d6916c415e32deda88bdcdf747a2f7bc7f8b1f9f
PJongu <akobke@yahoo.com>
2020-08-20 20:48:56 -0400

    Add project files.

---
+++ Noahandnear/NNcommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+
+namespace Noahandnear
+{
+
+    public class NNcommands
+    {
+        
+        [Command("hi")]
+       
+
+        public async Task Hi(CommandContext ctx)
+        {
+
+            await ctx.RespondAsync($"こんにちは！(・ω・)ノ {ctx.User.Mention}");
+            var interactivity = ctx.Client.GetInteractivity();
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Author.Id == ctx.User.Id && xm.Content.ToLower() == "how are you?", TimeSpan.FromMinutes(1));
+            if (msg != null)
+                await ctx.RespondAsync($"I'm fine, thank you!");
+
+        }
+        
+    }
+}
